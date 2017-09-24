2f4a67549722aa69c05dd5756e9665a823d073e4
Ayan George <ayan@boltcarrier.com>
2017-09-24 01:42:19 +0000

    First commit and first draft.  Needs proper testing, refactoring, and feedback.

---
+++ adstxt.go
@@ -0,0 +1,157 @@
+package adstxt
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"net/http"
+	"strings"
+	"sync"
+)
+
+type AdsTxt struct {
+	Source   string // URL of ads.txt file this represents.
+	Partner  []Buyer
+	Variable map[string][]string
+}
+
+type Buyer struct {
+	Domain                 string
+	PublisherID            string
+	AccountType            string
+	CertificationAuthority string
+}
+
+//
+// Attempts to split a comma separated buyer record.
+//
+func (a *AdsTxt) parseBuyerRecord(line string) error {
+	// if we made it here, we should look for a comma separated list.
+	col := strings.Split(line, ",")
+	if len(col) != 4 {
+		// Something is very wrong here.
+		return fmt.Errorf("Could not identify entity records.")
+	}
+	a.Partner = append(a.Partner,
+		Buyer{
+			Domain:                 strings.TrimSpace(col[0]),
+			PublisherID:            strings.TrimSpace(col[1]),
+			AccountType:            strings.TrimSpace(col[2]),
+			CertificationAuthority: strings.TrimSpace(col[3]),
+		})
+	return nil
+}
+
+// only two variables are supported: CONTACT and SUBDOMAIN.  lets check for
+// those.
+//
+// i'm not sure how to write a generic parser for the variabels because the
+// spec is ambiguous when it comes to if '=' can appear within the comma
+// separated entity record.  it is possible to confuse a valid entity with
+// a variable.
+//
+// so instead i only look for "CONTACT=" and "SUBDOMAINS=".
+//
+// naughty spec!
+//
+func (a *AdsTxt) parseVariable(line string) error {
+
+	for _, v := range []string{"CONTACT", "SUBDOMAIN"} {
+		tok := v + "="
+		if len(line) >= len(tok) && strings.ToUpper(line[:len(tok)]) == tok {
+			val := line[len(tok):]
+			a.Variable[v] = append(a.Variable[v], val)
+			return nil
+		}
+	}
+	return fmt.Errorf("Line does not conain a variable.")
+}
+
+func Parse(srcurl, txt string) (AdsTxt, error) {
+	rc := AdsTxt{
+		Source:   srcurl,
+		Variable: make(map[string][]string),
+	}
+
+	if txt == "" {
+		return rc, fmt.Errorf("Given an empty string.  Nothing to parse.")
+	}
+
+	// create a scanner that reads line by line.
+	scanner := bufio.NewScanner(strings.NewReader(txt))
+
+	for scanner.Scan() {
+		fmt.Printf(">> %s", scanner.Text())
+		line := strings.TrimSpace(scanner.Text())
+
+		// record is blank.
+		if line == "" {
+			continue
+		}
+
+		// record is a comment.
+		if line[0] == '#' {
+			continue
+		}
+
+		if rc.parseVariable(line) == nil {
+			continue
+		}
+
+		// at this point there is no need to check the result of
+		// parseBuyerRecord().  if it doesn't work then we have an invalid record
+		// and the loop will continue anyway.
+		rc.parseBuyerRecord(line)
+	}
+	return rc, nil
+}
+
+type fetchresult struct {
+	URL      string
+	Contents string
+}
+
+func fetch(url string, rc chan<- AdsTxt, wg *sync.WaitGroup) {
+	defer func() {
+		if wg != nil {
+			wg.Done()
+		}
+	}()
+
+	resp, err := http.Get(url)
+	defer resp.Body.Close()
+
+	if err != nil {
+		return
+	}
+
+	var buf bytes.Buffer
+	buf.ReadFrom(resp.Body)
+
+	a, _ := Parse(url, buf.String())
+	rc <- a
+}
+
+func FetchAdTxt(urls ...string) ([]AdsTxt, error) {
+	if len(urls) == 0 {
+		return nil, fmt.Errorf("No URLs supplied; nothing to do.")
+	}
+
+	results := make(chan AdsTxt, len(urls))
+	wg := sync.WaitGroup{}
+
+	wg.Add(len(urls))
+	for _, url := range urls {
+		go fetch(url, results, &wg)
+	}
+
+	wg.Wait()
+	close(results)
+
+	var rc []AdsTxt
+	for r := range results {
+		rc = append(rc, r)
+	}
+
+	return rc, nil
+}
---
+++ adstxt_test.go
@@ -0,0 +1,98 @@
+package adstxt
+
+import (
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+
+	tests := []struct {
+		Name     string
+		Domain   string
+		Contents string
+	}{
+		{
+			Name:   "Full Example #1",
+			Domain: "https://www.ayan.net/",
+			Contents: `# Ads.txt file for example.com:
+greenadexchange.com, 12345, DIRECT, d75815a79
+silverssp.com, 9675, RESELLER, f496211
+blueadexchange.com, XF436, DIRECT
+orangeexchange.com, 45678, RESELLER
+silverssp.com, ABE679, RESELLER`,
+		},
+		{
+			Name:   "Full Example #2",
+			Domain: "https://www.ayan.net/",
+			Contents: `# Ads.txt file for example.com:
+greenadexchange.com, 12345, DIRECT, d75815a79
+blueadexchange.com, XF436, DIRECT
+contact=adops@example.com
+contact=http://example.com/contact-us`,
+		},
+
+		{
+			Name:   "Full Example #3",
+			Domain: "https://www.ayan.net/",
+			Contents: `# Ads.txt file for example.com:
+greenadexchange.com, 12345, DIRECT, d75815a79
+blueadexchange.com, XF436, DIRECT
+subdomain=divisionone.example.com`,
+		},
+
+		{
+			Name:     "Contact Variable",
+			Domain:   "https://www.ayan.net/ads.txt",
+			Contents: "CONTACT=ayan@ayan.net",
+		}, {
+			Name:   "Multiple Contacts",
+			Domain: "https://www.ayan.net/ads.txt",
+			Contents: `CONTACT=ayan@ayan.net
+CONTACT=http://www.ayan.net`,
+		}, {
+			Name:   "Multiple Variables",
+			Domain: "https://www.ayan.net/ads.txt",
+			Contents: `CONTACT=ayan@ayan.net
+CONTACT=http://www.ayan.net
+SUBDOMAIN=goosgoarch.com
+CONTACT=ayan@goosgoarch.com
+`,
+		}, {
+			Name:     "First Test",
+			Domain:   "https://www.ayan.net/ads.txt",
+			Contents: "greenadexchange.com, XF7342, DIRECT, 5jyxf8k54",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			Parse(test.Domain, test.Contents)
+		})
+
+	}
+
+}
+
+func TestFetchURL(t *testing.T) {
+	c := make(chan AdsTxt)
+
+	go fetch("http://www.google.com", c, nil)
+
+	contents := <-c
+
+	t.Log("Contents: %#v", contents)
+}
+
+/*
+func TestFetchMultiplex(t *testing.T) {
+	url := []string{
+		"https://www.google.com",
+		"https://www.digg.com",
+		"https://www.reddit.com",
+	}
+
+	result := make(chan string, len(url))
+	wg := sync.WaitGroup{}
+
+}
+*/
